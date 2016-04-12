afbbea1eede92a0aefe6c7d118dace3a3ce31814
Marco Trevisan (Treviño) <mail@3v1n0.net>
2016-04-13 01:31:26 +0200

    webframe: override setZoomFactor and emit a zoom-factor-changed IPC when called
    When the webFrame zoom factor changes we need to get notified, so let's override
    the setter function.

--- webframe.js
+++ webframe.js
@@ -1,4 +1,18 @@
 var webFrame = require('web-frame');
+var ipc = require("ipc");
+
+(function() {
+  var webFrameSetZoomFactor = webFrame.setZoomFactor;
+  webFrame.setZoomFactor = function () {
+    var oldZoom = webFrame.getZoomFactor();
+    var ret = webFrameSetZoomFactor.apply(this, arguments);
+
+    if (oldZoom != webFrame.getZoomFactor())
+      ipc.send("zoom-factor-changed", webFrame.getZoomFactor());
+
+    return ret;
+  };
+})();
 
 window._zoomIn = function () {
   webFrame.setZoomFactor(webFrame.getZoomFactor() + 0.1);
