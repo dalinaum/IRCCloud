74a6977df51ed7f5cacc7cc101ab8104567db6a9
Marco Trevisan (Treviño) <mail@3v1n0.net>
2016-04-14 02:55:40 +0200

    webframe: add context menu to copy, paste or cut selections

--- webframe.js
+++ webframe.js
@@ -24,7 +24,6 @@ linkMenu.append(new MenuItem({
     window.open(document.activeElement.href);
   }
 }));
-
 linkMenu.append(new MenuItem({
   label: 'Copy Link',
   accelerator: 'CommandOrControl+C',
@@ -33,8 +32,47 @@ linkMenu.append(new MenuItem({
   }
 }));
 
+var copyMenu = new Menu();
+copyMenu.append(new MenuItem({
+  label: 'Copy',
+  accelerator: 'CommandOrControl+C',
+  role: 'copy'
+}));
+
+var pasteMenu = new Menu();
+pasteMenu.append(new MenuItem({
+  label: 'Paste',
+  accelerator: 'CommandOrControl+V',
+  role: 'paste'
+}));
+
+var inputSelectionMenu = new Menu();
+inputSelectionMenu.append(copyMenu.items[0]);
+inputSelectionMenu.append(new MenuItem({
+  label: 'Cut',
+  accelerator: 'CommandOrControl+X',
+  role: 'cut'
+}));
+inputSelectionMenu.append(pasteMenu.items[0]);
+
 window.addEventListener('contextmenu', function (event) {
-  if (document.activeElement.tagName == 'A' && document.activeElement.href.length > 0) {
+  var clickedElement = document.activeElement;
+  var anySelection = (document.getSelection().toString().length > 0);
+
+  if (clickedElement.tagName == 'INPUT' || clickedElement.tagName == 'TEXTAREA') {
+    event.preventDefault();
+    console.log(document.getSelection().toString())
+
+    if (!clickedElement.disabled) {
+      var menu = anySelection ? inputSelectionMenu : pasteMenu;
+      menu.popup(remote.getCurrentWindow());
+    } else if (anySelection) {
+      copyMenu.popup(remote.getCurrentWindow());
+    }
+  } else if (anySelection) {
+    event.preventDefault();
+    copyMenu.popup(remote.getCurrentWindow());
+  } else if (clickedElement.tagName == 'A' && clickedElement.href.length > 0) {
     event.preventDefault();
     linkMenu.popup(remote.getCurrentWindow());
   }
